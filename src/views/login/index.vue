b955658059e9828350526260b89c2a4a33731c0d
xu2327 <2327801183@qq.com>
2022-11-10 22:41:43 +0800

    feat: 开发登录页面

---
+++ src/views/login/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <!-- 账号 -->
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text" />
+      </el-form-item>
+      <!-- 密码 -->
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password" />
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <!-- 登录按钮 -->
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登录</el-button
+      >
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+// 导入组件之后无需注册可直接使用
+import { Avatar } from '@element-plus/icons'
+import {} from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
