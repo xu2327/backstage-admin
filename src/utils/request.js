952f037176f31ca0c4e09b9637d9873ac8c24c54
xu2327 <2327801183@qq.com>
2022-11-12 01:05:54 +0800

    feat: 登录，退出登录

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,42 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
+import store from '@/store'
+import { isCheckTimeout } from '@/utils/auth'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
 
+// 请求拦截器
+service.interceptors.request.use(
+  (config) => {
+    // 在这个位置需要统一的去注入token
+    if (store.getters.token) {
+      if (isCheckTimeout()) {
+        // 登出操作
+        store.dispatch('user/logout')
+        return Promise.reject(new Error('token 失效'))
+      }
+      // 如果token存在 注入token
+      config.headers.Authorization = `Bearer ${store.getters.token}`
+    }
+    return config // 必须返回配置
+  },
+  (error) => {
+    // 处理 token 超时问题
+    if (
+      error.response &&
+      error.response.data &&
+      error.response.data.code === 401
+    ) {
+      // token超时
+      store.dispatch('user/logout')
+    }
+    ElMessage.error(error.message) // 提示错误信息
+    return Promise.reject(error)
+  }
+)
 // 响应拦截器
 service.interceptors.response.use(
   (response) => {
