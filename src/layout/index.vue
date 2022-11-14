583c4f2f58d44c648ce0779dacf26d2dfd30bc20
xu2327 <2327801183@qq.com>
2022-11-14 19:20:36 +0800

    feat: contextMenu 时间处理

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -13,6 +13,8 @@
       <div class="fixed-header">
         <!-- 顶部的 navbar -->
         <navbar />
+        <!-- tags -->
+        <tags-view></tags-view>
       </div>
       <!-- 内容区 -->
       <app-main />
@@ -24,6 +26,7 @@
 import Navbar from './components/Navbar.vue'
 import Sidebar from './components/Sidebar'
 import AppMain from './components/AppMain.vue'
+import TagsView from '@/components/Tagsview'
 </script>
 
 <style lang="scss" scoped>
